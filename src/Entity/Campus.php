d1a7164aab1fe546a71d838c86b7e9ebd2252d02
loggeek <82963833+loggeek@users.noreply.github.com>
2024-09-02 15:50:36 +0200

    Ajout du campus de la sortie

--- src/Entity/Campus.php
+++ src/Entity/Campus.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CampusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CampusRepository::class)]
@@ -15,6 +17,17 @@ class Campus
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, Excursion>
+     */
+    #[ORM\OneToMany(targetEntity: Excursion::class, mappedBy: 'campus')]
+    private Collection $excursions;
+
+    public function __construct()
+    {
+        $this->excursions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Campus
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Excursion>
+     */
+    public function getExcursions(): Collection
+    {
+        return $this->excursions;
+    }
+
+    public function addExcursion(Excursion $excursion): static
+    {
+        if (!$this->excursions->contains($excursion)) {
+            $this->excursions->add($excursion);
+            $excursion->setCampus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExcursion(Excursion $excursion): static
+    {
+        if ($this->excursions->removeElement($excursion)) {
+            // set the owning side to null (unless already changed)
+            if ($excursion->getCampus() === $this) {
+                $excursion->setCampus(null);
+            }
+        }
 
         return $this;
     }
