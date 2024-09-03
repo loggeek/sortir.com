d6f1e940335f7bb36b7ba287bfd18117495508d3
vincent.ollivier1 <olliviev@gmail.com>
2024-09-03 11:24:20 +0200

    Création du formulaire de login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,16 +6,20 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
@@ -34,7 +38,7 @@ class User
     private ?string $password = null;
 
     #[ORM\Column]
-    private ?bool $isadmin = null;
+    private ?bool $isadmin = false;
 
     /**
      * @var Collection<int, Excursion>
@@ -198,5 +202,25 @@ class User
         }
 
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+//        $roles = $this->roles;
+//        // guarantee every user at least has ROLE_USER
+//        $roles[] = 'ROLE_USER';
+//
+//        return array_unique($roles);
+        return [];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
     }
 }
