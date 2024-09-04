bd01db57bd6bd127eade2a7c1b88ad26b89ee24d
vincent.ollivier1 <olliviev@gmail.com>
2024-09-04 11:08:20 +0200

    Ajout du lieu dans le formaulaire de créatio d'une excursion

---
+++ migrations/Version20240904082303.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240904082303 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE excursion ADD location_id INT NOT NULL');
+        $this->addSql('ALTER TABLE excursion ADD CONSTRAINT FK_9B08E72F64D218E FOREIGN KEY (location_id) REFERENCES location (id)');
+        $this->addSql('CREATE INDEX IDX_9B08E72F64D218E ON excursion (location_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+        $this->addSql('ALTER TABLE excursion DROP FOREIGN KEY FK_9B08E72F64D218E');
+        $this->addSql('DROP INDEX IDX_9B08E72F64D218E ON excursion');
+        $this->addSql('ALTER TABLE excursion DROP location_id');
+    }
+}
