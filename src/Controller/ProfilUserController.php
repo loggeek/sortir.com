79189d476685ab362bdca2db84a4744b0bafbc4e
loggeek <82963833+loggeek@users.noreply.github.com>
2024-09-06 10:22:12 +0200

    Problème d'import

--- src/Controller/ProfilUserController.php
+++ src/Controller/ProfilUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ProfilUserType;
 use App\Repository\CampusRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
