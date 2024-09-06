bb6cec071fd1d23d01a0ee27cb182a625363d068
Tay35 <andricktaylor1@gmail.com>
2024-09-06 11:27:07 +0200

    Gestion de l'image du profile

--- src/Form/ProfilUserType.php
+++ src/Form/ProfilUserType.php
@@ -7,12 +7,14 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -68,7 +70,24 @@ class ProfilUserType extends AbstractType
                     'choice_label' => 'name',
                     'label' => 'Campus',
                     'required' => false,
-            ]);
+            ])
+            ->add('profileImage', FileType::class, [
+                'label' => 'Ma photo',
+//                'placeholder' => 'Télécharger vers le serveur (JPEG, PNG)',
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veillez télécharger une image valide en JPEG ou en PNG',
+                    ]),
+                ],
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
