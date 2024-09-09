a85ae2d4aa560c380ed4b0f6e521f97fe01aef4f
Logan Letellier <loggeek.top@gmail.com>
2024-09-09 10:33:38 +0200

    Nettoyage du contrôleur admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -25,24 +25,17 @@ class AdminController extends AbstractController
     #[Route('/campus/{filter}', name: 'campus_filter')]
     public function filterCampus(string $filter, CampusRepository $campusRepository): Response
     {
-        $sites = $campusRepository->findAll();
-        $sites = array_filter($sites,
-            fn ($campus) => str_contains(strtoupper($campus->getName()), strtoupper($filter))
-        );
-
         return $this->render('admin/campus.html.twig', [
-            'sites' => $sites
+            'sites' => $campusRepository->findAndFilter($filter)
         ]);
     }
 
     #[Route('/campus/add/{nom}', name: 'campus_add')]
     public function addCampus(string $nom, CampusRepository $campusRepository, EntityManagerInterface $em): Response
     {
-        foreach ($campusRepository->findAll() as $campus) {
-            if ($campus->getName() === $nom) {
-                // TODO: avertir que le campus existe déjà
-                return $this->redirectToRoute('admin_campus');
-            }
+        if ($campusRepository->findOneByName($nom)) {
+            // vérifier que le nouveau nom du campus ne soit pas déjà attribué
+            return $this->redirectToRoute('admin_campus');
         }
 
         $campus = new Campus();
@@ -58,50 +51,31 @@ class AdminController extends AbstractController
     {
         $sites = $campusRepository->findAll();
 
-        // vérifier que le nouveau nom du campus ne soit pas déjà attribué
-        foreach ($campusRepository->findAll() as $campus) {
-            if ($campus->getName() === $nouveau) {
-                // TODO: avertir que le campus existe déjà
-                return $this->redirectToRoute('admin_campus');
-            }
-        }
-
-        // trouver le campus à modifier
-        // c'est moche mais ça fait l'affaire - bouger ça dans le service ?
-        $campus = (function() use ($sites, $ancien) {
-            foreach ($sites as $campus) {
-                if ($campus->getName() === $ancien) {
-                    return $campus;
-                }
-            }
-            return null;
-        })();
-
-        // vérifier qu'il existe bel et bien
-        if ($campus == null) {
-            // TODO: avertir que le campus n'existe pas
+        if ($campusRepository->findOneByName($nouveau)) {
+            // vérifier que le nouveau nom du campus ne soit pas déjà attribué
             return $this->redirectToRoute('admin_campus');
         }
 
-        // modifier le campus
-        $campus->setName($nouveau);
-        $em->persist($campus);
-        $em->flush();
+        $campus = $campusRepository->findOneByName($ancien);
+
+        if ($campus) {
+            $campus->setName($nouveau);
+            $em->persist($campus);
+            $em->flush();
+        }
 
         return $this->redirectToRoute('admin_campus');
     }
 
-
     #[Route('/campus/delete/{nom}', name: 'campus_delete')]
     public function deleteCampus(string $nom, CampusRepository $campusRepository, EntityManagerInterface $em): Response
     {
-        foreach ($campusRepository->findAll() as $campus) {
-            if ($campus->getName() === $nom) {
-                $em->remove($campus);
-            }
-        }
+        $campus = $campusRepository->findOneByName($nom);
 
-        $em->flush();
+        if ($campus) {
+            $em->remove($campus);
+            $em->flush();
+        }
 
         return $this->redirectToRoute('admin_campus');
     }
