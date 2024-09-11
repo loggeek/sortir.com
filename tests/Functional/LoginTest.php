9fd9fa0e6c51c8bd9548615ffec5298052ca4cac
vincent.ollivier1 <olliviev@gmail.com>
2024-09-11 14:38:42 +0200

    Ajout des commentaires sur les tests

--- tests/Functional/LoginTest.php
+++ tests/Functional/LoginTest.php
@@ -10,23 +10,32 @@ class LoginTest extends WebTestCase
 {
     public function testLoginSuccesful(): void
     {
+        // Création d'un client HTTP
         $client = static::createClient();
 
+        // Obtention du générateur d'URL
         /*** @var UrlGeneratorInterface $urlGenerator */
         $urlGenerator = $client->getContainer()->get('router');
 
+        // Génération de l'URL de la route de connexion
         $crawler = $client->request('GET', $urlGenerator->generate('app_login'));
 
+        // Soumission du formulaire de connexion
         $form = $crawler->filter("form[name=login]")->form([
             "_username" => "Vince",
             "_password" => "111111"
         ]);
 
+        // Soumission du formulaire
         $client->submit($form);
+
+        // Vérification du code de statut de la réponse
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
 
+        // Suivi de la redirection
         $client->followRedirect();
 
+        // Vérification de la route finale
         $this->assertRouteSame('app_home');
 
     }
